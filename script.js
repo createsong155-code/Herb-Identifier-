4f51bcf281d8185e9653b323ff3eaae5c651c705
createsong155-code <createsong123@gmail.com>
2025-11-15 16:25:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,6 @@ const herbs = [
   "images": [
     { "part": "Leaf", "url": "alagaw-leaf.jpg" },
     { "part": "Plant", "url": "alagaw-plant.jpg" }
-     
   ],
   "searchTerms": [
     "alagaw", "premna", "respiratory", "fever",
